fc37f01fe7602739b442180d5811caa5baefe3b8
Mateus Faria <webmat1@hotmail.com>
2020-02-02 16:29:54 -0300

    Project Startup.

---
+++ MyFirstGame/MyFirstGame/Hubs/Hub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace MyFirstGame.Hubs
+{
+    public class Hub : Microsoft.AspNetCore.SignalR.Hub
+    {
+        public void SendNumber(int Number)
+        {
+            Clients.Others.SendAsync("UpdateNumber", Number);
+        }
+    }
+}
